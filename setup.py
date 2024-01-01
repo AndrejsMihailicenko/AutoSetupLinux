869b8c47b7639434a70013e2a518ddcb32b30506
Andrejs <andrejsmihailicenko@gmail.com>
2024-01-01 16:44:34 +0200

    experimental changes,try to  make code better

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 import subprocess
+from bs4 import BeautifulSoup
 
+def read_packages():
+    apps_list = []
+    with open("packages.csv", "r") as f:
+        next(f)
+        for line in f:
+            if line:
+                apps_list.append(line[0])
+    return apps_list
+            
 def system_update():
-    subprocess.run('sudo dnf update && sudo dnf upgrade', shell=True, check=True)
+    subprocess.run('sudo dnf update -y && sudo dnf upgrade -y', shell=True, check=True)
 
-def apps_installation():
-    pass
+def get_last_version():
+    pass #web scr. to get latest packages
+
+def apps_installation(apps_list):
+    for app in apps_list:
+        subprocess.run(f'sudo dnf install -y {app}', shell=True, check=True)
 
 if __name__ == "__main__":
     system_update()
+    apps_list = read_packages() 
+    apps_installation(apps_list) 
