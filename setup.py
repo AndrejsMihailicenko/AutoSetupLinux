29d66c408b0ca9c03779bb94ca3e1a695c7f3e15
Andrejs <andrejsmihailicenko@gmail.com>
2024-01-02 18:27:38 +0200

    feat: java install

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import subprocess
-from bs4 import BeautifulSoup
+import bs4
 import distro
 import sys
+import requests
+
 
 def os_detector():
     name = distro.name() 
@@ -10,8 +12,15 @@ def os_detector():
     elif (name == 'Debian GNU/Linux' or name == 'Ubuntu'):
         return 'apt'
     else:
-        sys.exit()  
-    
+        sys.exit('Unsupported operating system')  
+
+def install_libraries(package_manager):
+    if (package_manager == 'dnf'):
+        subprocess.run('sudo dnf install -y python3-pip', shell=True)
+    if (package_manager == 'apt'):
+        subprocess.run('sudo apt install -y python3-pip', shell=True)
+    subprocess.run('pip install -r requirements.txt', shell=True, check=True)
+
 def read_packages():
     apps_list = []
     with open("packages.csv", "r") as f:
@@ -24,8 +33,24 @@ def read_packages():
 def system_update(package_manager):
     subprocess.run(f'sudo {package_manager} update -y && sudo {package_manager} upgrade -y', shell=True, check=True)
 
-def get_last_version(package_manager):
-    pass 
+def install_jdk(package_manager):
+    url = 'https://www.oracle.com/java/technologies/downloads/'
+    r = requests.get(url)
+    if r.status_code == 200:
+        page_content = bs4.BeautifulSoup(r.content, 'html.parser')
+
+        if package_manager == 'dnf':
+            download_link = page_content.find('a', href=lambda href: href and "jdk-21_linux-x64_bin.rpm" in href)
+            if download_link:
+                download_url = download_link['href']
+                subprocess.run(f'wget {download_url}', shell=True)
+                subprocess.run(f'sudo dnf install -y {download_url.split('/')[-1]}', shell=True)
+        elif package_manager == 'apt':
+            download_link = page_content.find('a', href=lambda href: href and "jdk-21_linux-x64_bin.deb" in href)
+            if download_link:
+                download_url = download_link['href']
+                subprocess.run(f'wget {download_url}', shell=True)
+                subprocess.run(f'sudo dpkg -i {download_url.split('/')[-1]}', shell=True)
 
 def apps_installation(package_manager, apps_list):
     for app in apps_list:
@@ -33,6 +58,8 @@ def apps_installation(package_manager, apps_list):
 
 if __name__ == "__main__":
     package_manager = os_detector()
-    system_update()
+    install_libraries(package_manager)
+    system_update(package_manager)
     apps_list = read_packages() 
-    apps_installation(apps_list) 
+    apps_installation(apps_list)
+    install_jdk(package_manager)
